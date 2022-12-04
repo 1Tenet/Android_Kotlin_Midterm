9fa952673381220d5a473666ef6bb165086c67ee
driuft <air.socarras@gmail.com>
2022-12-04 12:17:03 -0500

    first commit

--- app/src/main/java/com/driuft/androidkotlindebugging/ui/HelloWorldActivity.kt
+++ app/src/main/java/com/driuft/androidkotlindebugging/ui/HelloWorldActivity.kt
@@ -3,10 +3,14 @@ package com.driuft.androidkotlindebugging.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.driuft.androidkotlindebugging.R
+import android.content.Intent
+import android.widget.Button
+
 
 class HelloWorldActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hello_world)
+
     }
 }
